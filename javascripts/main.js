d688aac8e18107d71472c96c0250f864df1304af
Ede Success <edesuccess54@gmail.com>
2022-07-05 14:01:31 +0100

    first commit

---
+++ javascripts/main.js
@@ -0,0 +1,16 @@
+
+const openNav = document.querySelector('.hamburger');
+const toggleoverlay = document.querySelector('.toggleoverlay');
+const toggleNav = document.querySelector('.toggle-nav');
+const closeNav = document.querySelector('#close');
+
+function openCloseNav() {
+    toggleNav.classList.toggle('toggle-active');
+    toggleoverlay.classList.toggle('toggleoverlay-active');
+}
+
+openNav.addEventListener('click', openCloseNav);
+
+closeNav.addEventListener('click', openCloseNav);
+
+toggleoverlay.addEventListener('click', openCloseNav)
